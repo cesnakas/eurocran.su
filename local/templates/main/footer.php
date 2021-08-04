2e5101dfc99ce1615ada2ccec61f4b855b38a6d0
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-08-04 22:52:36 +0300

    Update footer.php

--- local/templates/main/footer.php
+++ local/templates/main/footer.php
@@ -8,27 +8,28 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
                 <div class="row">
                     <div class="footer__logo">
                         <a href="<?=SITE_DIR?>"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/logo-footer.svg"/></a>
-                        <p>Аренда мобильных и <br> гусеничных кранов LIEBHERR</p><b>© 2000-2021 Все права защищены</b>
+                        <p>Аренда мобильных и <br> гусеничных кранов LIEBHERR</p>
+                        <b>© 2000-<?=date('Y')?> Все права защищены</b>
                     </div>
                     <div class="footer__item">
                         <h3>Клиентам</h3>
                         <ul>
-                            <li><a href="<?=SITE_DIR?>">О компании</a></li>
-                            <li><a href="<?=SITE_DIR?>">Услуги</a></li>
-                            <li><a href="<?=SITE_DIR?>">Каталог техники</a></li>
-                            <li><a href="<?=SITE_DIR?>">Аренда кранов</a></li>
-                            <li><a href="<?=SITE_DIR?>">Наши работы</a></li>
-                            <li><a href="<?=SITE_DIR?>">Прайс-лист</a></li>
-                            <li><a href="<?=SITE_DIR?>">Контакты</a></li>
+                            <li><a href="<?=SITE_DIR?>o-kompanii/">О компании</a></li>
+                            <li><a href="<?=SITE_DIR?>uslugi/">Услуги</a></li>
+                            <li><a href="<?=SITE_DIR?>tehnika/">Каталог техники</a></li>
+                            <li><a href="<?=SITE_DIR?>avtokrany/">Аренда кранов</a></li>
+                            <li><a href="<?=SITE_DIR?>objekty/">Наши работы</a></li>
+                            <li><a href="<?=SITE_DIR?>prais-list/">Прайс-лист</a></li>
+                            <li><a href="<?=SITE_DIR?>kontakty/">Контакты</a></li>
                         </ul>
                     </div>
                     <div class="footer__item">
                         <h3>Техника</h3>
                         <ul>
-                            <li><a href="<?=SITE_DIR?>">Гусеничные краны</a></li>
-                            <li><a href="<?=SITE_DIR?>">Мобильные краны</a></li>
-                            <li><a href="<?=SITE_DIR?>">Низкорамные тралы</a></li>
-                            <li><a href="<?=SITE_DIR?>">Модульные платформы</a></li>
+                            <li><a href="<?=SITE_DIR?>tehnika/apenda-gusenichnykh-kpanov-liebherr-lr/">Гусеничные краны</a></li>
+                            <li><a href="<?=SITE_DIR?>tehnika/apenda-mobilnykh-avtokpanov-liebherr-ltm/">Мобильные краны</a></li>
+                            <li><a href="<?=SITE_DIR?>tehnika/arenda-nizkoramnykh-tralov/">Низкорамные тралы</a></li>
+                            <li><a href="<?=SITE_DIR?>tehnika/arenda-modulnykh-platform/">Модульные платформы</a></li>
                         </ul>
                     </div>
                     <div class="footer__item2">
@@ -42,7 +43,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
                         </ul>
                     </div>
                     <div class="footer__actions">
-                        <a class="btn btn--full" href="">Свяжитесь со мной</a>
+                        <a class="btn btn--full" href="#callback-form">Свяжитесь со мной</a>
                         <ul>
                             <li><a href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/insta.svg"/></a></li>
                             <li><a href="#"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/twitter.svg"/></a></li>
@@ -53,9 +54,9 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
                     </div>
                     <div class="footer__contacts">
                         <h3>Контакты</h3>
-                        <a href="tel:+7 (499)929-96-66">+7 (499)929-96-66 </a>
-                        <a href="tel:+7 (985)364-55-18">+7 (985)364-55-18 </a>
-                        <a href="tel:+7 (985)226-20-30">+7 (985)226-20-30 </a>
+                        <a href="tel:+74999299666">+7 (499)929-96-66</a>
+                        <a href="tel:+79853645518">+7 (985)364-55-18</a>
+                        <a href="tel:+79852262030">+7 (985)226-20-30</a>
                     </div>
                 </div>
             </div>
