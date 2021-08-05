9d5511c674e17fc541a02e3545806703bcb9b37c
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-08-06 02:11:15 +0300

    update component lease

---
+++ local/templates/main/components/bitrix/news.list/lease/template.php
@@ -0,0 +1,61 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global \CMain $APPLICATION */
+/** @global \CUser $USER */
+/** @global \CDatabase $DB */
+/** @var \CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var array $templateData */
+/** @var \CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+
+<div class="lease">
+	<div class="container">
+
+		<div class="g-title">
+			<h1> <span>Спецтехника </span> в аренду</h1>
+		</div>
+
+	</div>
+
+	<div class="lease__slider" id="lease__slider">
+
+		<?foreach($arResult['ITEMS'] as $arItem):?>
+		<div class="lease__item">
+			<div class="row">
+				<div class="lease__img">
+                    <?=CFile::ShowImage(
+                        $arItem['PREVIEW_PICTURE']['SRC'],
+                        650,
+                        460,
+                        'style="width: 650px; height: 460px; object-fit: cover; object-position: center;" alt="'.$arItem['NAME'].'"',
+                        $arItem["DETAIL_PAGE_URL"],
+                        false
+                    );?>
+				</div>
+				<div class="lease__text">
+					<div class="g-title">
+						<h3><?=$arItem['NAME']?></h3>
+					</div>
+					<?=$arItem['PREVIEW_TEXT']?>
+					<!--<p>Аренда гусеничного крана – актуальная услуга для многих строительных площадок. Использование спецтехники существенно упрощает погрузку - выгрузку материалов, оптимизирует рабочий процесс.</p>-->
+					<div class="lease__btn">
+                        <a class="btn btn--full" href="<?=$arItem["DETAIL_PAGE_URL"]?>">Выбрать спецтехнику</a>
+                    </div>
+				</div>
+			</div>
+		</div>
+        <?endforeach;?>
+
+	</div>
+
+	<div class="lease__arrows"></div>
+	<div class="lease__dots"></div>
+
+</div>
--- ~index.php
+++ ~index.php
@@ -64,7 +64,72 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 	<!--END PROMO-->
 
 	<!--START LEASE-->
-	<div class="lease">
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:news.list",
+        "lease",
+        Array(
+			"IBLOCK_TYPE" => "catalog",
+			"IBLOCK_ID" => "1",
+			"NEWS_COUNT" => "10",
+			"SORT_BY1" => "ACTIVE_FROM",
+			"SORT_ORDER1" => "DESC",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER2" => "ASC",
+			"FILTER_NAME" => "arrFilter",
+			"FIELD_CODE" => Array(
+                // 'ID',
+                '1',
+                '2',
+                '4'
+            ),
+			"PROPERTY_CODE" => Array("DESCRIPTION"),
+
+            // "DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			// "AJAX_MODE" => "Y",
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"SET_TITLE" => "N",
+			"SET_BROWSER_TITLE" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "Y",
+			"PARENT_SECTION" => "",
+			"PARENT_SECTION_CODE" => "",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"CACHE_TYPE" => "A",
+			"CACHE_TIME" => "3600",
+			"CACHE_FILTER" => "Y",
+			"CACHE_GROUPS" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"DISPLAY_BOTTOM_PAGER" => "N",
+			"PAGER_TITLE" => "Новости",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => "",
+			"PAGER_DESC_NUMBERING" => "Y",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "Y",
+			"PAGER_BASE_LINK_ENABLE" => "Y",
+			"SET_STATUS_404" => "Y",
+			"SHOW_404" => "Y",
+			"MESSAGE_404" => "",
+			"PAGER_BASE_LINK" => "",
+			"PAGER_PARAMS_NAME" => "arrPager",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_ADDITIONAL" => ""
+        ),
+    false
+    );?>
+	<!--<div class="lease">
 		<div class="container">
 			<div class="g-title">
 				<h1> <span>Спецтехника </span> в аренду</h1>
@@ -73,7 +138,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 		<div class="lease__slider" id="lease__slider">
 			<div class="lease__item">
 				<div class="row">
-					<div class="lease__img"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/lease1.jpg"/>
+					<div class="lease__img"><img src="<?/*=SITE_TEMPLATE_PATH*/?>/img/content/lease1.jpg"/>
 					</div>
 					<div class="lease__text">
 						<div class="g-title">
@@ -86,7 +151,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 			</div>
 			<div class="lease__item">
 				<div class="row">
-					<div class="lease__img"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/lease1.jpg"/>
+					<div class="lease__img"><img src="<?/*=SITE_TEMPLATE_PATH*/?>/img/content/lease1.jpg"/>
 					</div>
 					<div class="lease__text">
 						<div class="g-title">
@@ -99,7 +164,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 			</div>
 			<div class="lease__item">
 				<div class="row">
-					<div class="lease__img"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/lease1.jpg"/>
+					<div class="lease__img"><img src="<?/*=SITE_TEMPLATE_PATH*/?>/img/content/lease1.jpg"/>
 					</div>
 					<div class="lease__text">
 						<div class="g-title">
@@ -112,7 +177,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 			</div>
 			<div class="lease__item">
 				<div class="row">
-					<div class="lease__img"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/lease1.jpg"/>
+					<div class="lease__img"><img src="<?/*=SITE_TEMPLATE_PATH*/?>/img/content/lease1.jpg"/>
 					</div>
 					<div class="lease__text">
 						<div class="g-title">
@@ -125,7 +190,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 			</div>
 			<div class="lease__item">
 				<div class="row">
-					<div class="lease__img"><img src="<?=SITE_TEMPLATE_PATH?>/img/content/lease1.jpg"/>
+					<div class="lease__img"><img src="<?/*=SITE_TEMPLATE_PATH*/?>/img/content/lease1.jpg"/>
 					</div>
 					<div class="lease__text">
 						<div class="g-title">
@@ -139,7 +204,7 @@ $APPLICATION->SetTitle("Аренда автокранов и спецтехни�
 		</div>
 		<div class="lease__arrows"></div>
 		<div class="lease__dots"></div>
-	</div>
+	</div>-->
 	<!--END LEASE-->
 
 	<!--START RELIABLE-->
