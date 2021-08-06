56f1d7997902c11ebfd92f1edb4414248ebb4878
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-08-06 18:03:24 +0300

    Update menu

--- .main.menu.php
+++ .main.menu.php
@@ -57,4 +57,3 @@ $aMenuLinks = Array(
 		"" 
 	)
 );
-?>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -116,6 +116,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 							);?>
                         </ul>
                     </div>
+                    <?/*
                     <div class="nav__search">
                         <div class="search">
                             <form action="<?=SITE_DIR?>search/">
@@ -126,6 +127,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
                             </form>
                         </div>
                     </div>
+                    */?>
                 </div>
             </div>
         </div>
