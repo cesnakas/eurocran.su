79734dab6848d6195f2a912e7f2f697c7318e9d5
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-08-28 15:38:23 +0400

    Update page catalog

--- catalog/~index.php
+++ catalog/~index.php
@@ -37,33 +37,30 @@ $APPLICATION->IncludeComponent(
 			<div class="g-title">
 				<h2>Нужна помощь в подборе? </h2>
 			</div>
-			<form action="">
-				<div class="row">
-					<input type="text" placeholder="Имя">
-					<input type="text" placeholder="Телефон">
-					<label class="btn btn--full">Получить консультацию
-						<input type="submit">
-					</label>
-				</div>
-			</form>
+			<?php
+			$APPLICATION->IncludeComponent(
+				'cesnakas:main.feedback',
+				'main.technique',
+				[
+					'USE_CAPTCHA' => 'N',
+					'OK_TEXT' => 'Спасибо, ваше сообщение принято.',
+					'EMAIL_TO' => 'evro-k@yandex.ru',
+					'REQUIRED_FIELDS' => ['NAME','PHONE'],
+					'EVENT_MESSAGE_ID' => ['7']
+				]
+			);?>
 		</div>
 	</div>
 	<!--END PUT-THE-TECHNIQUE-->
 
     <div class="product-single__content">
         <div class="container">
-            <h2>Заголовок</h2>
-            <p>Гусеничный кран LR 1750 находит свое применение в строительстве электростанций, нефтеперерабатывающих заводов, на строительных площадках мостов и при монтаже ветросиловых установок. </p>
-            <p>Эта 750-тонная машина имеет универсальное применение со своей 140-метровой основной стрелой, 105-метровым управляемым удлинителем стрелы, 21-метровым монтажным удлинителем, деррик-оборудованием и 400-тонным подвесным противовесом или балластировочной тележкой. </p>
-            <p>Компактные габариты компонентов подъемного крана и их умеренный индивидуальный вес позволяют обеспечить экономичную транспортировку этого подъемного крана к строительной площадке. </p>
-            <p>Этот 750-тонный кран может также использоваться с выдвижными опорами, имеющими опорную базу 13 x 13 м. Результирующая грузоподъемность в этом случае может быть увеличена до 370 %.</p>
-            <p>Гусеничный кран LR 1750 находит свое применение в строительстве электростанций, нефтеперерабатывающих заводов, на строительных площадках мостов и при монтаже ветросиловых установок. </p>
-            <p>Эта 750-тонная машина имеет универсальное применение со своей 140-метровой основной стрелой, 105-метровым управляемым удлинителем стрелы, 21-метровым монтажным удлинителем, деррик-оборудованием и 400-тонным подвесным противовесом или балластировочной тележкой. </p>
-            <p>Компактные габариты компонентов подъемного крана и их умеренный индивидуальный вес позволяют обеспечить экономичную транспортировку этого подъемного крана к строительной площадке. </p>
-            <p>Этот 750-тонный кран может также использоваться с выдвижными опорами, имеющими опорную базу 13 x 13 м. Результирующая грузоподъемность в этом случае может быть увеличена до 370 %.Гусеничный кран LR 1750 находит свое применение в строительстве электростанций, нефтеперерабатывающих заводов, на строительных площадках мостов и при монтаже ветросиловых установок. </p>
-            <p>Эта 750-тонная машина имеет универсальное применение со своей 140-метровой основной стрелой, 105-метровым управляемым удлинителем стрелы, 21-метровым монтажным удлинителем, деррик-оборудованием и 400-тонным подвесным противовесом или балластировочной тележкой. </p>
-            <p>Компактные габариты компонентов подъемного крана и их умеренный индивидуальный вес позволяют обеспечить экономичную транспортировку этого подъемного крана к строительной площадке. </p>
-            <p>Этот 750-тонный кран может также использоваться с выдвижными опорами, имеющими опорную базу 13 x 13 м. Результирующая грузоподъемность в этом случае может быть увеличена до 370 %.</p>
+			<?php
+            $APPLICATION->IncludeFile(
+				SITE_TEMPLATE_PATH.'/include/catalog/product-content.php',
+				[],
+				['SHOW_BORDER' => true, 'MODE' => 'text']
+			);?>
         </div>
     </div>
 
@@ -75,33 +72,73 @@ $APPLICATION->IncludeComponent(
             </div>
             <div class="row">
                 <div class="contacts__info">
-                    <p>Ежедневно мы развиваемся и стараемся стать лучше для вас, поэтому всегда рады получить от вас обратную связь с пожеланиями и идеями по улучшению наших продуктов!</p>
-                    <p>Если у вас остались вопросы или есть предложения как стать лучше пишите нам!</p>
-                    <div class="contacts__loc"><i class="icon"> <img src="img/content/location.svg"/></i><span> г. Москва, Россия, 117628,м. Бульвар Дмитрия Донского, ул. Куликовская, 12</span></div>
+                    <div class="contacts__loc">
+                        <i class="icon">
+                            <svg width="24" height="24" fill="#FCB427" role="img" aria-label="Location">
+                                <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/images/icons.svg#location"/>
+                            </svg>
+                        </i>
+                        <span> г. Москва, Россия, 117628,м. Бульвар Дмитрия Донского, ул. Куликовская, 12</span>
+                    </div>
                     <div class="contacts__row">
-                        <div class="contacts__tel"><i class="icon"><img src="img/content/phone.svg"/></i><span> <a href="tel:+74999299666">+7 (499)929-96-66</a><a href="tel:+79853645518">+7 (985)364-55-18</a><a href="tel:+79852262030">+7 (985)226-20-30</a></span></div>
+                        <div class="contacts__tel">
+                            <i class="icon">
+                                <svg width="24" height="24" fill="#FCB427" role="img" aria-label="Phone">
+                                    <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/images/icons.svg#phone"/>
+                                </svg>
+                            </i>
+                            <span>
+                                <a href="tel:+74999299666">+7 (499)929-96-66</a>
+                                <a href="tel:+79853645518">+7 (985)364-55-18</a>
+                                <a href="tel:+79852262030">+7 (985)226-20-30</a>
+                            </span>
+                        </div>
                         <div class="contacts__other">
-                            <div class="contacts__mail"><i class="icon"> <img src="img/content/mail.svg"/></i><a href="mailto:evro-k@yandex.ru">evro-k@yandex.ru</a></div>
-                            <div class="contacts__schedule"><i class="icon"> <img src="img/content/clock.svg"/></i><span>10:00 - 17:00</span></div>
+                            <div class="contacts__mail">
+                                <i class="icon">
+                                    <svg width="24" height="24" fill="#FCB427" role="img" aria-label="Mail">
+                                        <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/images/icons.svg#mail"/>
+                                    </svg>
+                                </i>
+                                <a href="mailto:evro-k@yandex.ru">evro-k@yandex.ru</a>
+                            </div>
+                            <div class="contacts__schedule">
+                                <i class="icon">
+                                    <svg width="24" height="24" fill="#FCB427" role="img" aria-label="Time">
+                                        <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/images/icons.svg#workTime"/>
+                                    </svg>
+                                </i>
+                                <span>10:00 - 17:00</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contacts__row2">
+                        <h3>Реквизиты:</h3>
+                        <div class="contacts__tel">
+                            <i class="icon">
+                                <svg width="24" height="24" fill="#FCB427" role="img" aria-label="Doc">
+                                    <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/images/icons.svg#doc"/>
+                                </svg>
+                            </i>
+                            <span>
+                                <b>ООО "Компания Еврокран"</b><b>ИНН: 7718999293</b><b>ОГРН: 5147746130593</b>
+                            </span>
                         </div>
                     </div>
                 </div>
                 <div class="contacts__form">
-                    <form action="">
-                        <input type="text" placeholder="Ваше имя">
-                        <div class="form__row">
-                            <input type="tel" placeholder="Ваш телефон*">
-                            <input type="email" placeholder="Ваша почта">
-                        </div>
-                        <textarea name="" placeholder="Оставьте ваш вопрос"></textarea>
-                        <label class="checkbox">
-                            <input type="checkbox" checked="checked">
-                            <div class="input"></div><span>Я согласен с <a href=""> условиями обработки </a> и использования моих персональных данных</span>
-                        </label>
-                        <label class="btn btn--full">Оставить заявку
-                            <input type="submit">
-                        </label>
-                    </form>
+					<?php
+					$APPLICATION->IncludeComponent(
+						'cesnakas:main.feedback',
+						'.default',
+						[
+							'USE_CAPTCHA' => 'N',
+							'OK_TEXT' => 'Спасибо, ваше сообщение принято.',
+							'EMAIL_TO' => 'evro-k@yandex.ru',
+							'REQUIRED_FIELDS' => Array('NAME','PHONE','EMAIL','MESSAGE'),
+							'EVENT_MESSAGE_ID' => Array('7')
+						]
+					);?>
                 </div>
             </div>
         </div>
