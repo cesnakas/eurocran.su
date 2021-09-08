b1c6f22ae4dccf552c36fa749ce80a1f24143a1a
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-09-08 14:18:16 +0400

    Update urlrewrite

---
+++ urlrewrite.php
@@ -0,0 +1,91 @@
+<?php
+$arUrlRewrite=array (
+  15 => 
+  array (
+    'CONDITION' => '#^/dev/tehnika#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/dev/tehnika/index.php',
+    'SORT' => 100,
+  ),
+  13 => 
+  array (
+    'CONDITION' => '#^={SITE_DIR."stati/"}#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/dev/stati/index.php',
+    'SORT' => 100,
+  ),
+  14 => 
+  array (
+    'CONDITION' => '#^/catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/catalog/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/objekty/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/objekty/index.php',
+    'SORT' => 100,
+  ),
+  11 => 
+  array (
+    'CONDITION' => '#^/tehnika/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/tehnika/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/uslugi/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/uslugi/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/stati/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/stati/index.php',
+    'SORT' => 100,
+  ),
+  9 => 
+  array (
+    'CONDITION' => '#^\\??(.*)#',
+    'RULE' => '&$1',
+    'ID' => 'bitrix:catalog.smart.filter',
+    'PATH' => '/bitrix/templates/.default/components/bitrix/news/products/section.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/tech/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/tech/index.php',
+    'SORT' => 100,
+  ),
+  12 => 
+  array (
+    'CONDITION' => '#^/test/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/test/index.php',
+    'SORT' => 100,
+  ),
+  8 => 
+  array (
+    'CONDITION' => '#^/new/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/new/index.php',
+    'SORT' => 100,
+  ),
+);
